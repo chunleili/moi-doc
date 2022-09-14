66ea5a10ab3040746e1610dcb631cb2026f76433
chunleili <li_cl@foxmail.com>
2022-09-14 14:31:39 +0800

    edit this page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/chunleili/moi-doc',
+            'https://github.com/chunleili/moi-doc/tree/master',
         },
         blog:false,
         theme: {
